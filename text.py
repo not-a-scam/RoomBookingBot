ca2be2c6a61b6812dea447787133a14890f67409
Zechariah Yeo <70231898+not-a-scam@users.noreply.github.com>
2025-09-10 00:19:22 +0800

    Update text.py

--- text.py
+++ text.py
@@ -1,24 +1,24 @@
 START_MSG = "Welcome to the COOS Youth Room Booking Bot!"
 
 #Booking Convo Text
-NAME_MSG = """Welcome to the COOS Youth Room Booking Bot\! 
+NAME_MSG = """Welcome to the COOS Youth Room Booking Bot! 
 You may type /cancel at anytime to exit booking
 
-*Enter the name to be booked under*"""
-PAX_MSG = "*Enter the expected no\. of people using room*"
-DATE_MSG = """*Enter the date\(s\) room should be booked* \(e\.g\. 12 Feb, 30 jan\)
+<b>Enter the name to be booked under</b>"""
+PAX_MSG = "<b>Enter the expected no. of people using room</b>"
+DATE_MSG = """<b>Enter the date(s) room should be booked</b> (e.g. 12 Feb, 30 jan)
 
-if you have reccuring dates\, just write your requirements \(e\.g\. every tuesday from 12 Feb to 30 Feb\)"""
-TIME_MSG = "*Enter time room should be booked for* \(e\.g\. 430pm \- 6pm\)"
-PURPOSE_MSG = "*Enter the reason for room booking*"
-SCREEN_MSG = "*Do you need a screen\?* \(Enter yes or no\)"
-REQUEST_MSG = "*Enter extra requests or NA if none*"
-BOOKED_MSG = "*Your room booking request has been sent\!*"
+if you have reccuring dates, just write your requirements (e.g. every tuesday from 12 Feb to 30 Feb)"""
+TIME_MSG = "<b>Enter time room should be booked for</b> (e.g. 430pm - 6pm)"
+PURPOSE_MSG = "<b>Enter the reason for room booking</b>"
+SCREEN_MSG = "<b>Do you need a screen?</b> (Enter yes or no)"
+REQUEST_MSG = "<b>Enter extra requests or NA if none</b>"
+BOOKED_MSG = "<b>Your room booking request has been sent!</b>"
 
 CANCEL_MSG = "Operation Cancelled"
 
-NAME_CHECK_MSG = """Welcome to the COOS Youth Room Booking Bot\! 
+NAME_CHECK_MSG = """Welcome to the COOS Youth Room Booking Bot! 
 You may type /cancel at anytime to exit checking
 
-*Enter the name to be checked under*"""
-CHECKED_MSG = "*Your room checking request has been sent\!*"
+<b>Enter the name to be checked under</b>"""
+CHECKED_MSG = "<b>Your room checking request has been sent!</b>"
